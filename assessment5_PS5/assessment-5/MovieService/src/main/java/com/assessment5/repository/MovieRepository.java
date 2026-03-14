4ee6da65f83ce5a2f05bca31f3852e0025d5518f
Ritika <119058491+funsook@users.noreply.github.com>
2026-03-14 20:47:06 +0530

    Delete assessment5_PS5/assessment-5 directory

--- assessment5_PS5/assessment-5/MovieService/src/main/java/com/assessment5/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assessment5.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assessment5.model.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie, Long>{
-
-}
